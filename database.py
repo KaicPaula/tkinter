0a6a77b495293208e7b999c6f5933dbe1b2dd613
KaicPaula <keickzinho01@gmai.com>
2026-04-16 13:21:03 -0300

    conexao e validaçao da interface login com o banco de dados

--- database.py
+++ database.py
@@ -2,11 +2,82 @@ import sqlite3
 def inicializar_banco():
     conexao = sqlite3.connect("banco.db")
     cursor = conexao.cursor()
-    cursor.execute("""CREATE TABLE IF NOT EXISTS nomes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome TEXT NOT NULL,
-        data_entrada TEXT NOT NULL
-                )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS funcionarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    senha TEXT NOT NULL,
+    cargo TEXT -- 'Dono' ou 'Vendedor'
+    )
+    """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS veiculos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    cor TEXT NOT NULL,
+    ano TEXT NOT NULL,
+    valor_fipe FLOAT,
+    valor_compra_ou_cliente FLOAT, -- O que a loja pagou ou o cliente da revenda quer
+    comissao_sugerida FLOAT,
+    status TEXT DEFAULT 'Disponível' -- 'Disponível' ou 'Vendido'
+)""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS vendas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_veiculo INTEGER NOT NULL,
+    id_vendedor INTEGER NOT NULL,
+    valor_de_compra_ou_cliente FLOAT NOT NULL,
+    valor_final_venda FLOAT NOT NULL,
+    data_venda TEXT,
+    FOREIGN KEY (id_veiculo) REFERENCES veiculos (id),
+    FOREIGN KEY (id_vendedor) REFERENCES funcionarios (id)
+)""")
 
     conexao.commit()
     conexao.close()
+
+class NovoVeiculo:
+    def __init__(self,nome, cor, ano, valor_fipe, valor_compra_ou_cliente, comissao_sugerida, status):
+        self.nome = nome
+        self.cor = cor
+        self.ano = ano
+        self.valor_fipe = valor_fipe
+        self.valor_compra_ou_cliente = valor_compra_ou_cliente
+        self.comissao_sugerida = comissao_sugerida
+        self.status = status
+        
+class NovoFuncionario:
+    def __init__(self, nome, senha, cargo):
+        self.nome = nome
+        self.senha = senha
+        self.cargo = cargo
+        
+    def salvar_funcionario_db(self):
+        conexao = sqlite3.connect("banco.db")
+        cursor = conexao.cursor()
+        dados = (
+            self.nome,
+            self.senha,
+            self.cargo
+        )
+        cursor.execute("""INSERT INTO funcionarios
+                       (nome, senha, cargo) VALUES
+                       (?,?,?)""", dados)
+        conexao.commit()
+        
+def consultar_login(nome_login, senha_login):
+    conexao = sqlite3.connect("banco.db")
+    cursor = conexao.cursor()
+    cursor.execute("""SELECT nome, senha FROM funcionarios WHERE nome = ?""",(nome_login,))
+    dados = cursor.fetchone()
+    if dados == None:
+        return None
+    elif dados[1] != senha_login:
+        return False
+    elif dados[0] == nome_login and dados[1] == senha_login:
+        return True
+    # se o nome existe no banco de dados retorna None
+    # se a senha nao coincide retorna False
+    # Se os dados  forem exato, retorna True
+        
+
+        
+    
+    
--- main.py
+++ main.py
@@ -1,22 +1,38 @@
 import customtkinter as ctk
-from database import inicializar_banco
+import database as db
+from time import sleep
 
 
-inicializar_banco()
+db.inicializar_banco()
 
-banco = [
-    ("kaic"),
-    ("paula"),
-    ("duda")
-]
+# dados manual:
+# kaic = db.NovoFuncionario("kaic", "1122", "dono")
+# kaic.salvar_funcionario_db()
 
-def funcao_frame():
-    verificar_texto = entry_teste.get()
-    if verificar_texto in banco:
+
+# ------------------------------------------
+
+# def funcao_frame():
+#     verificar_texto = entry_teste.get()
+#     if verificar_texto in banco:
+#         pagina_login.pack_forget()
+#         pagina_sistema.pack()
+#     else:
+#         label_erro.configure(text="Nome invalido")
+
+def validar_login():
+    nome_login = entrada_nome.get() 
+    senha_login = entrada_senha.get()
+    retorno_consulta = db.consultar_login(nome_login, senha_login)
+    if retorno_consulta == None:
+        label_pagina_login.configure(text="Nome invalido")
+    elif retorno_consulta == False:
+        label_pagina_login.configure(text="Senha invalida")
+    elif retorno_consulta == True:
+        sleep(1)
         pagina_login.pack_forget()
         pagina_sistema.pack()
-    else:
-        label_erro.configure(text="Nome invalido")
+        
         
         
 def copiar_nome(nome):
@@ -26,33 +42,37 @@ def copiar_nome(nome):
 
 janela = ctk.CTk()
 janela.title("Pagina de login")
-janela.geometry("800x600")
+janela.geometry("400x300")
 
 pagina_login = ctk.CTkFrame(janela)
 pagina_login.pack()
 
-label = ctk.CTkLabel(pagina_login, text="Testando")
+label = ctk.CTkLabel(pagina_login, text="Login")
 label.pack()
-entry_teste = ctk.CTkEntry(pagina_login,placeholder_text="Testando")
-entry_teste.pack()
-botao_frame = ctk.CTkButton(pagina_login, text="Entrar", command=funcao_frame)
-botao_frame.pack()
-label_erro = ctk.CTkLabel(pagina_login, text=" ")
-label_erro.pack()
+entrada_nome = ctk.CTkEntry(pagina_login,placeholder_text="Nome de login")
+entrada_nome.pack(pady=10)
+entrada_senha = ctk.CTkEntry(pagina_login, placeholder_text="Senha", show="*")
+entrada_senha.pack(pady=10)
+botao_confirmar_login = ctk.CTkButton(pagina_login, text="Acessar", command=validar_login)
+botao_confirmar_login.pack()
+label_pagina_login = ctk.CTkLabel(pagina_login, text="")
+label_pagina_login.pack(pady=10)
 
+# ----------------------- JANELA SISTEMA ---------------------------------
 pagina_sistema = ctk.CTkFrame(janela)
 ctk.CTkLabel(pagina_sistema, text="Bem vindo ao sistema").pack()
 
-scroll = ctk.CTkScrollableFrame(pagina_sistema, width=200, height=150) # frame scrollavel
-scroll.pack(padx=10, pady=10)
 
-for i in banco:
-    nome_atual = i
-    ctk.CTkLabel(scroll, text=f"nome: {i}").pack() # widget dentro do escrolavel
-    botao_nome = ctk.CTkButton(scroll,text="Copiar Nome", command= lambda nome=i: copiar_nome(nome))
-    botao_nome.pack()
+# scroll = ctk.CTkScrollableFrame(pagina_sistema, width=200, height=150) # frame scrollavel
+# scroll.pack(padx=10, pady=10)
+
+# for i in banco:
+#     nome_atual = i
+#     ctk.CTkLabel(scroll, text=f"nome: {i}").pack() # widget dentro do escrolavel
+#     botao_nome = ctk.CTkButton(scroll,text="Copiar Nome", command= lambda nome=i: copiar_nome(nome))
+#     botao_nome.pack()
 """
-Lambda faz a paausa do loop pra poder criar um botao independente pra cada nome,
+Lambda faz a pausa do loop pra poder criar um botao independente pra cada nome,
 pra q cada botao possa enviar o nome (nesse caso) para a funçao poder tratar o dado.
 """
     
